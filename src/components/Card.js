1484ec52d8ffcc6557c0fa662aeaff74e0809e47
Derek Alia <derekalia@me.com>
2017-07-27 22:48:39 -0700

    removed start

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Tabs } from '../routes';
+import { Text, View,Button } from 'react-native';
+
+class Card extends React.Component {
+  render() {
+    return (
+      <View><Text>Pasta</Text></View>
+    );
+  }
+}
+
+export default Card;
