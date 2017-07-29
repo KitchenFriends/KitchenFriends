197d9085dd25ec640559a7a22ebe16c374c6a9e3
Derek Alia <derekalia@me.com>
2017-07-28 21:30:10 -0700

    re-orginized the tabs and added share

---
+++ src/screens/Share.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native'
+
+class Share extends React.Component{
+    render(){
+        return(
+            <Text>This is the share page</Text>
+        )
+    }
+}
+
+export default Share;
+
