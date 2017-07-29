3d9dbfb1a280c2ae017f51089b407784f11ef3cd
Derek Alia <derekalia@me.com>
2017-07-28 23:24:55 -0700

    added shareStack to sharetab

---
+++ src/screens/Direction.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+class Direction extends React.Component {
+  render() {
+    return (
+      
+        <View>
+            <Text>Direction</Text>
+        </View>      
+    );
+  }
+}
+
+export default Direction;
---
+++ src/screens/Ingredient.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+class Ingredient extends React.Component {
+  render() {
+    return (
+      
+        <View>
+            <Text>Ingredient!</Text>
+        </View>      
+    );
+  }
+}
+
+export default Ingredient;
